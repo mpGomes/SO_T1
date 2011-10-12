f56acc310563eb3c39996cc127879934c65b0265
goncalopp <goncalopp@pekkala.(none)>
2011-10-12 21:34:47 +0100

    factorized test1

--- modified_files_dir/root/t1/test1.c
+++ modified_files_dir/root/t1/test1.c
@@ -1,21 +1,29 @@
 #include <sys/lfsyscall.h> 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+char buffer[1024];
+
+void send(char* msg)
+    {
+    int rv;
+    int size= strlen(msg) +1;
+    printf("SEND: sending %d bytes of '%s' \n", size, msg);
+    rv= lfsyscall(0, msg, size);
+    printf("SEND: returned %d \n", rv);
+    }
+
+void receive(int size)
+    {
+    int rv;
+    printf("RECEIVE: receiving\n");
+    rv=lfsyscall(1, buffer, size);
+    printf("RECEIVE: returned %d \n", rv);
+    printf("RECEIVE: received '%s' \n", buffer);
+    }
 void main()
 {   
-    int return_value;
-    char* sent_message= "bla\0";
-    char* received_message= malloc(10);
-    
-    printf("sending '%s' \n", sent_message);
-    return_value= lfsyscall(0, sent_message, 4);
-    printf("returned %d \n", return_value);
-    
-    printf("receiving\n");
-    return_value=lfsyscall(1, received_message, 4);
-    printf("returned %d \n", return_value);
-    printf("received '%s' \n", received_message);
-    
-    printf("all done! \n");
+    send("bla");
+    receive(5);
 }
