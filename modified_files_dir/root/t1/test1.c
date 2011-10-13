bb1efe3098b6123865506d90b9059a9ff253845e
goncalopp <goncalopp@pekkala.(none)>
2011-10-13 16:36:41 +0100

    test modifications

--- modified_files_dir/root/t1/test1.c
+++ modified_files_dir/root/t1/test1.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 char buffer[1024];
 
@@ -9,21 +11,114 @@ void send(char* msg)
     {
     int rv;
     int size= strlen(msg) +1;
-    printf("SEND: sending %d bytes of '%s' \n", size, msg);
+    fprintf(stderr, "SEND: sending %d bytes of '%s' \n", size, msg);
     rv= lfsyscall(0, msg, size);
-    printf("SEND: returned %d \n", rv);
+    fprintf(stderr, "SEND: returned %d \n", rv);
     }
 
-void receive(int size)
+int receive(int size)
     {
     int rv;
-    printf("RECEIVE: receiving\n");
+    fprintf(stderr, "RECEIVE: receiving\n");
     rv=lfsyscall(1, buffer, size);
-    printf("RECEIVE: returned %d \n", rv);
-    printf("RECEIVE: received '%s' \n", buffer);
+    fprintf(stderr, "RECEIVE: returned %d \n", rv);
+    fprintf(stderr, "RECEIVE: received '%s' \n", buffer);
+    return rv;
     }
+    
+void fixed_itoa(int n, char* str)
+    {
+    char result[10]= "000000000";
+    char buffer[10];
+    int len;
+    sprintf(buffer,"%d",n);
+    len= strlen(buffer);
+    strcpy(&result[9-len], buffer);
+    strcpy(str, result);
+    }
+    
+void test_wait_queues()
+    {
+    int pid, i;
+    int iterations= 5;
+    char msg[10];
+    msg[1]='\0';
+    
+    pid= fork();
+    if (pid==0)
+        {
+        msg[0]= '0';
+        for (i=0; i<iterations; i++)
+            {
+            msg[0]+=1;
+            send(msg);
+            printf("sent message\n");
+            }
+        }
+    else
+        {
+        for (i=0; i<iterations; i++)
+            {
+            receive(1000);
+            printf("received message\n");
+            sleep(1);
+            }
+        }
+        
+    printf("-------------------------------\n");
+    
+    pid= fork();
+    if (pid==0)
+        {
+        msg[0]= '0';
+        for (i=0; i<iterations; i++)
+            {
+            msg[0]+=1;
+            send(msg);
+            printf("sent message\n");
+            sleep(1);
+            }
+        }
+    else
+        {
+        for (i=0; i<iterations; i++)
+            {
+            receive(1000);
+            printf("received message\n");
+            }
+        }
+        
+        
+    }
+    
+void test_concurrency()
+    {
+    int i;
+    int iterations= 1000;
+    char msg[10];
+    
+
+    if (fork()==0)
+        {
+        fixed_itoa(i, msg);
+        for (i=0; i<iterations; i++)
+            {
+            send(msg);
+            printf("%s sent\n", msg);
+            }
+        }
+    if (fork()==0)
+        {
+        for (i=0; i<iterations; i++)
+            {
+            receive(10);
+            printf("%s received\n", buffer);
+            }
+        }
+    }
+
 void main()
-{   
-    send("bla");
-    receive(5);
-}
+    {   
+    //test_concurrency();
+    test_wait_queues();
+    }
